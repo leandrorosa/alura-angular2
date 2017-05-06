4c33f5fb547fefcf7dcd6b3794913896959f1e5f
Leandro Rosa <leandro.rosagf@gmail.com>
2017-05-06 14:24:03 -0300

    aula 3

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,4 +1,6 @@
-import { NgModule } from '@angular/core'
+import 'rxjs/add/operator/map';
+
+import { NgModule } from '@angular/core';
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import {FotoModule} from "./foto/foto.module";
