b9e0e6ea10bab7c0b58a6be58daf695101e8559c
Leandro Rosa <leandro.rosagf@gmail.com>
2017-05-21 15:39:21 -0300

    Aula 5

--- client/app/foto/foto.pipes.ts
+++ client/app/foto/foto.pipes.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {FotoComponent} from "./foto.component";
 
 @Pipe ({
     name: 'filtroPorTitulo'
 }) 
 export class FiltroPorTitulo implements PipeTransform {
     
-    transform(fotos: any, digitado) {
-        console.log(fotos);
-        console.log(digitado);        
+    transform(fotos: FotoComponent[], digitado:string) {
+        return fotos.filter(foto => foto.titulo.toLowerCase().includes(digitado.toLowerCase()));
     }
 
 }
