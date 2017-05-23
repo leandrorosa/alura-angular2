59e3e26867124a5c527f8d79ecb92f5e57be2d26
Leandro Rosa <leandro.rosa@neogrid.com>
2017-05-23 13:18:55 -0300

    Implementando aula 6

--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -1,11 +1,17 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 
 @Component({
     moduleId: module.id,
     selector: 'painel',
     templateUrl: "painel.component.html"
 })
-export  class PainelComponent {
+export  class PainelComponent implements OnInit {
 
     @Input() titulo:string;
+
+    ngOnInit(): void {
+        if(this.titulo.length > 7) {
+            this.titulo = `${this.titulo.substr(0, 7)}...`;
+        }
+    }
 }
